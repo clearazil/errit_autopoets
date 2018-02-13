abdc669ef4b9ab096adeebe71d847e96700ccd4b
Derk van der Heide <derkvanderheide@hotmail.com>
2018-02-14 00:44:23 +0100

    set tty false

--- deploy.php
+++ deploy.php
@@ -16,7 +16,7 @@ set('bin/console', '{{release_path}}/bin/console');
 set('repository', getenv('REPOSITORY_URL'));
 
 // [Optional] Allocate tty for git clone. Default value is false.
-set('git_tty', true);
+set('git_tty', false);
 
 // Shared files/dirs between deploys
 add('shared_files', []);
